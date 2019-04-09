4b4cf891017ebd7d5a0c458765da1e9451c01da6
gideonschaller <gideonschaller@hotmail.com>
2019-04-09 14:09:08 -0700

    Create GapLastHourCount.sql

---
+++ Monitoring/GapLastHourCount.sql
@@ -0,0 +1,4 @@
+select count(*)
+from ea_sc_kif.sc_order_details
+where unix_timestamp(timestamp(hbase_timestamp), 'yyyy-MM-dd HH:mm:ss') - unix_timestamp(serial, 'yyyyMMddHHmmss') > 300 
+and timestamp(hbase_timestamp) > (unix_timestamp(CURRENT_TIMESTAMP) - 3600)
